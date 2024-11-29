d6aca29aad3b8dd9e5a79020958ec0563ec61072
tinalin728 <yuting.lin728@gmail.com>
2024-11-28 22:38:46 -0800

    edited homepage animation

--- portfolio/src/pages/Crafts.jsx
+++ portfolio/src/pages/Crafts.jsx
@@ -3,16 +3,14 @@ import PageHero from '../components/PageHero'
 import HorizontalScroll from '../components/HorizontalScroll'
 import CraftCard from '../components/CraftCard'
 
+import data from './home/data.json';
+
 function Crafts() {
     const [crafts, setCrafts] = useState([])
     useEffect(() => {
-        fetch('/data.json')
-            .then(response => response.json())
-            .then(data => {
-                const { crafts } = data;
-                setCrafts(crafts);
-            });
-    }, [])
+        const { crafts } = data;
+        setCrafts(crafts);
+    }, []);
 
     return (
         <>
@@ -21,8 +19,8 @@ function Crafts() {
                 tagline='Made With Love & Purpose'
             />
 
-            <section className=' py-10 border-y border-black'>
-                <div className='grid grid-cols-1 md:grid-cols-2 border-t border-black bg-light-grey-bg'>
+            <section className=' py-10 border-y-2 border-black'>
+                <div className='grid grid-cols-1 md:grid-cols-2 border-t-2 border-black bg-light-grey-bg'>
                     {crafts.map((craft) => (
                         <CraftCard
                             key={craft.id}
@@ -30,7 +28,7 @@ function Crafts() {
                             title={craft.title}
                             img={craft.img}
                             skills={craft.skills}
-                            border={`${craft.id === 1 || craft.id === 3 ? 'md:border-r' : ''}`}
+                            border={`${craft.id === 1 || craft.id === 3 ? 'md:border-r-2' : ''}`}
                         />
                     ))}
                 </div>
