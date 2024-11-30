9c23ebf68702504a8150a218f833ed9681057ea4
tinalin728 <yuting.lin728@gmail.com>
2024-11-30 14:46:20 -0800

    update crafts

--- portfolio/src/pages/Crafts.jsx
+++ portfolio/src/pages/Crafts.jsx
@@ -26,7 +26,7 @@ function Crafts() {
 
 
     const filterClasses = (isActive) =>
-        `py-1 px-4 rounded-full shadow-charcoal border-2 hover:shadow-charcoal-hover transition duration-300 ${isActive ? 'bg-charcoal shadow-none text-white' : 'bg-white text-charcoal'
+        `font-roundo-medium text-base py-1 px-8 rounded-t-xl border-t-2 border-x-2 transition duration-300 ${isActive ? 'bg-charcoal text-white' : 'bg-light-yellow-bg text-charcoal'
         }`
 
 
@@ -37,13 +37,13 @@ function Crafts() {
                 tagline='Made With Love & Purpose'
             />
 
-            <section className=' border-black'>
+            <section className='pb-10'>
                 <div className='hidden md:block'>
-                    <div className='max-w-container flex gap-4 mb-8'>
+                    <div className='ml-10 flex'>
                         <button
                             className={filterClasses(filter === 'all')}
                             onClick={() => setFilter('all')}
-                        >All </button>
+                        > All </button>
 
                         <button
                             className={filterClasses(filter === 'UXUI')}
@@ -56,30 +56,30 @@ function Crafts() {
                         >Front-End </button>
                     </div>
                 </div>
+                <div className='py-10 border-2  bg-light-grey-bg'>
+                    <div className="grid grid-cols-1 md:grid-cols-2 border-2 ">
+                        {filteredCrafts.slice(0, 4).map((craft, index) => {
+                            const columns = 2;
+                            const isSingleColumn = windowWidth < 768;
+                            const isLastRow = isSingleColumn
+                                ? index === crafts.length - 1
+                                : index >= crafts.length - (crafts.length % columns || columns);
 
-                <div className="grid grid-cols-1 md:grid-cols-2 border-y-2 mb-10">
-                    {crafts.slice(0, 4).map((craft, index) => {
-                        const columns = 2;
-                        const isSingleColumn = windowWidth < 768;
-                        const isLastRow = isSingleColumn
-                            ? index === crafts.length - 1
-                            : index >= crafts.length - (crafts.length % columns || columns);
-
-                        const bottomBorder = !isLastRow ? "border-b-2" : "";
-                        const rightBorder = !isSingleColumn && index % columns === 0 ? "md:border-r-2" : "";
-                        return (
-                            <CraftCard
-                                key={craft.id}
-                                id={craft.id}
-                                title={craft.title}
-                                img={craft.img}
-                                skills={craft.skills}
-                                border={`${bottomBorder} ${rightBorder}`}
-                            />
-                        );
-                    })}
+                            const bottomBorder = !isLastRow ? "border-b-2" : "";
+                            const rightBorder = !isSingleColumn && index % columns === 0 ? "md:border-r-2" : "";
+                            return (
+                                <CraftCard
+                                    key={craft.id}
+                                    id={craft.id}
+                                    title={craft.title}
+                                    img={craft.img}
+                                    skills={craft.skills}
+                                    border={`${bottomBorder} ${rightBorder}`}
+                                />
+                            );
+                        })}
+                    </div>
                 </div>
-
             </section >
 
         </>
