5397d25fc30f1fcc9308e1c6827e9a35420ea843
tinalin728 <yuting.lin728@gmail.com>
2024-11-29 23:07:13 -0800

    lenis in conflict with modal stilll in fix

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Crafts from './pages/Crafts'
@@ -7,6 +7,8 @@ import Layout from './layouts/Layout'
 import CraftDetail from './pages/CraftDetail'
 import ScrollToTop from './hooks/ScrollToTop'
 import SmoothScrolling from './components/SmoothScrolling'
+import { ParallaxProvider } from "react-scroll-parallax";
+
 
 
 function App() {
@@ -14,16 +16,20 @@ function App() {
   return (
     <Router>
       <SmoothScrolling>
-        <ScrollToTop />
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/crafts' element={<Crafts />} />
-            <Route path='/crafts/:id' element={<CraftDetail />} />
-          </Route>
-        </Routes>
-      </SmoothScrolling>
+        <ParallaxProvider>
+
+          <ScrollToTop />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/crafts' element={<Crafts />} />
+              <Route path='/crafts/:id' element={<CraftDetail />} />
+            </Route>
+          </Routes>
+        </ParallaxProvider>
+
+      </SmoothScrolling >
     </Router>
   )
 }
--- portfolio/src/components/ImageModal.jsx
+++ portfolio/src/components/ImageModal.jsx
@@ -1,24 +1,57 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useLenis } from 'lenis/react';
 
 function ImageModal({ isOpen, src, alt, onClose }) {
+    const lenis = useLenis();
+
+    useEffect(() => {
+
+        if (isOpen) {
+            // Disable body scroll
+            lenis?.stop();
+            document.body.style.overflow = 'auto';
+            document.querySelector('#modal-container').style.overflow = 'auto'; // Enable modal scroll
+
+
+        } else {
+            // Re-enable body scroll
+            lenis?.start();
+            document.body.style.overflow = '';
+        }
+
+        // Cleanup when the component unmounts
+        return () => {
+            lenis?.start();
+            document.body.style.overflow = '';
+        };
+    }, [isOpen, lenis]);
 
     if (!isOpen) return null;
 
     return (
-        <div className='fixed inset-0 z-50 w-full h-full overflow-auto bg-black bg-opacity-80 pt-[80px]' onClick={onClose}>
-
-            <div className='relative max-w-[85rem] mx-auto h-auto scroll py-4 px-12'>
-                <img src={src} alt={alt} className='mx-auto transition duration-300 animation-zoom'
-                    onClick={(e) => e.stopPropagation()}
+        <div data-scroll
+            id='modal-container'
+            className="fixed inset-0 z-50 w-full h-auto bg-black bg-opacity-80"
+            onClick={onClose}
+        >
+            <div
+                className="relative max-w-[85rem] mx-auto h-auto overflow-auto py-4 px-12"
+                onClick={(e) => e.stopPropagation()} // Prevent click from closing modal
+            >
+                <img
+                    src={src}
+                    alt={alt}
+                    className="block mx-auto transition duration-300 animation-zoom"
                 />
-
             </div>
-            <button className='absolute top-10 right-10 text-white text-2xl'>
+            <button
+                className="absolute top-10 right-10 text-white text-2xl"
+                onClick={onClose}
+            >
                 &times;
             </button>
         </div>
-    )
-
+    );
 }
 
-export default ImageModal
+export default ImageModal;
