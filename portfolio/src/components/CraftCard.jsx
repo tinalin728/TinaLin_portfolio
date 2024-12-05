99eb0a0b43bda10c7fe7c643bd29b3d3501b3241
tinalin728 <yuting.lin728@gmail.com>
2024-12-04 16:21:01 -0800

    mouse

--- portfolio/src/components/CraftCard.jsx
+++ portfolio/src/components/CraftCard.jsx
@@ -5,7 +5,6 @@ import figma from '../../public/assets/about/figma.svg'
 function CraftCard({ title, img, skills, id, craftHeaderRef }) {
 
     const [scale, setScale] = useState(1);
-    const [position, setPosition] = useState({ x: 0, y: 0 })
 
     const handleMouseMove = (e) => {
 
@@ -16,26 +15,19 @@ function CraftCard({ title, img, skills, id, craftHeaderRef }) {
         setScale(1)
     }
 
-    // self-stretch shrink-0 h-auto (img)
 
 
     return (
         <>
             <Link to={`/crafts/${id} `}
-                className={`relative font-bold leading-normal capitalize w-full h-full box group border-2 rounded-xl bg-light-yellow-bg`}
+                className='relative font-bold leading-normal capitalize w-full h-full box group border-2 rounded-xl bg-light-yellow-bg'
                 onMouseMove={handleMouseMove} onMouseOut={handleMouseOut}>
-                {/* 
-                <span className="absolute block bg-black transform duration-500 ease-in-out top-0 left-0 w-full h-[2.2px] origin-left scale-x-0 group-hover:scale-x-100 z-10  rounded-lg"></span>
-                <span className="absolute block bg-black transform duration-500 ease-in-out top-0 right-0 w-[2.2px] h-full origin-top scale-y-0 group-hover:scale-y-100 "></span>
-                <span className="absolute block bg-black transform duration-500 ease-in-out bottom-0 right-0 w-full h-[2.2px] origin-right scale-x-0 group-hover:scale-x-100 "></span>
-                <span className="absolute block bg-black transform duration-500 ease-in-out left-0 top-0 w-[2.2px] h-full origin-bottom scale-y-0 group-hover:scale-y-100 "></span> */}
 
-                {/* p-[1.5rem] lg:p-[2rem]  */}
-                <div className="z-10 relative p-[1rem]" >
+                <div className="flex flex-col p-[1rem]" >
                     <div className='overflow-hidden border-2 rounded-xl' >
                         <img src={img} alt="project"
                             className='relative object-cover max-w-full h-auto transition duration-500 ease-in-out'
-                            style={{ transform: `scale(${scale}) translate(-${position.x}%, -${position.y}%)` }}
+                            style={{ transform: `scale(${scale})` }}
                         />
                     </div>
 
@@ -50,9 +42,9 @@ function CraftCard({ title, img, skills, id, craftHeaderRef }) {
                             ))}
                         </div>
 
-                        <div className="absolute right-0">
+                        <div className="block absolute right-0 md:bottom-4">
                             <div className="bg-charcoal w-fit text-white px-4 rounded-l-xl group-hover:bg-orange transition duration-500">
-                                <p className="text-sm md:text-base">
+                                <p className="text-sm py-1 lg:text-base">
                                     Read Me
                                 </p>
                             </div>
